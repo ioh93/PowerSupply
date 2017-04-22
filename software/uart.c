b710a4ada98b36ce714b9b81de0d47941a32a869
ioh93 <gothic.pomykacz199@gmail.com>
2017-04-22 17:30:18 +0200

    UART update
    Implemented basic functionality

--- software/uart.c
+++ software/uart.c
@@ -9,13 +9,43 @@
 #include uart.h
 
 /*!
-* @brief A description of function
-* @param[in] a Description of a
-* @param[in] b Description of b
-* @return int8_t
+* @brief UART initialisation
+* @param[in] ubrr Value to set in UBRRn register, that determines baud rate.
+*                 Macro defined in uart.h
+* @return null
 */
-//int8_t
-//max (int8_t /*a*/, int8_t /*b*/)
-//{
-//  return ((a > b) ? a : b);
-//}
+void uartInit(uint8_t ubrr)
+{
+    // Set UART baudrate
+    UBRR0H = (uint8_t)(ubrr >> 8);
+    UBRR0L = (uint8_t)ubrr;
+    // Enable receiver and transmitter
+    UCSR0B = (1 << RXEN0) | ( 1<< TXEN0);
+    // Set frame format: 8data, 2stop bit
+    UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+}
+
+/*!
+* @brief UART basic transmit function
+* @param[in] data Data to be transmitted
+* @return null
+*/
+void uartTransmit(uint8_t data)
+{
+    // Wait for empty transmit buffer
+    while ( !(UCSRnA & (1 << UDREn)));
+    // Put data into buffer, sends the data
+    UDRn = data;
+}
+
+/*!
+* @brief UART basic recieve function
+* @return uint8_t
+*/
+uint8_t uartReceive(void)
+{
+    // Wait for data to be received
+    while ( !(UCSRnA & (1 << RXCn)));
+    // Get and return received data from buffer
+    return UDRn;
+}
